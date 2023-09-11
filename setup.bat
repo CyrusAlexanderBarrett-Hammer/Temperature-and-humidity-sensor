90373a81a652bfc4be8b73379df3f5f3b1f39133
Cyrus Alexander Barrett-Hammer <144144187+CyrusAlexanderBarrett-Hammer@users.noreply.github.com>
2023-09-11 17:30:59 +0200

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,10 +1,10 @@
-@echo off
+::@echo off
 setlocal
 
 :: Define variables
 set PYTHON_INSTALLER_URL=https://www.python.org/ftp/python/3.6.8/python-3.6.8.exe
 set PYTHON_INSTALLER_PATH=%USERPROFILE%\Downloads\python-3.6.8.exe
-set GET_PIP_URL=https://bootstrap.pypa.io/get-pip.py
+set GET_PIP_URL=https://bootstrap.pypa.io/pip/3.6/get-pip.py
 set GET_PIP_PATH=%TEMP%\get-pip.py
 set PYTHON_PATH=%USERPROFILE%\AppData\Local\Programs\Python\Python36-32
 set PIP_PATH=%PYTHON_PATH%\Scripts\pip.exe
@@ -39,7 +39,14 @@ pause
 
 :: Install Python 3.6.8
 echo Installing Python 3.6.8...
-start /wait "" %PYTHON_INSTALLER_PATH% /quiet InstallAllUsers=0 PrependPath=0 TargetDir=%PYTHON_PATH%
+echo Installer path is %PYTHON_INSTALLER_PATH%
+echo Installation path is %PYTHON_PATH%
+start /wait "" %PYTHON_INSTALLER_PATH% /quiet InstallAllUsers=0 PrependPath=1 TargetDir=%PYTHON_PATH%
+echo Installation error level: %ERRORLEVEL%
+
+if exist %PYTHON_INSTALLER_PATH% (echo Thank god)
+
+pause
 
 :: Download get-pip.py
 ::echo Downloading get-pip.py...
@@ -47,30 +54,39 @@ start /wait "" %PYTHON_INSTALLER_PATH% /quiet InstallAllUsers=0 PrependPath=0 Ta
 
 if %USE_THIS_MAJOR_VERSION% geq 10 (
     echo Downloading get-pip.py using curl...
+    pause
     curl -o %GET_PIP_PATH% %GET_PIP_URL%
 ) else (
+    pause
     echo Downloading get-pip.py using bitsadmin...
     bitsadmin /transfer myDownloadJob /download /priority normal %GET_PIP_URL% %CD%\%GET_PIP_PATH%
 )
 
+pause
+
 :: Install pip
-::echo Installing pip...
-::%PYTHON_PATH%\python.exe %GET_PIP_PATH%
+echo Installing pip...
+%PYTHON_PATH%\python.exe %GET_PIP_PATH%
 
 :: Add Python and pip to PATH
-set NEW_PATH=%PATH%;%PYTHON_PATH%;%PYTHON_PATH%"
+echo Possible crash coming up? Setting path next.
+pause
+set NEW_PATH=%PATH%;%PYTHON_PATH%\Scripts
+echo Path set...
+pause
 if defined NEW_PATH if "%NEW_PATH:~0,1024%" neq "%NEW_PATH%" (
     echo Error: New PATH exceeds the 1024 character limit imposed by setx.
-    PATH is over 1028 characters long, and MAY not work on older systems/programs. Press any key to continue.
+    PATH is over 1028 characters long, so the installation will not work. Ask IT to disable PATH limit, then try again. Press any key to continue.
     pause
+    exit
 )
 setx PATH "%NEW_PATH%"
 
 :: Install Python libraries
 echo Installing required Python libraries...
-pip install pyserial
-pip install tk
-
+%PIP_PATH% install pyserial
+%PIP_PATH% install serial
+%PIP_PATH% install tk
 pause
 
 :: Clean up
